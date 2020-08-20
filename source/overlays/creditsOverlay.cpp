55c59df09e30b74a9b446e2b340e73954f034530
StackZ <47382115+SuperSaiyajinStackZ@users.noreply.github.com>
2020-08-20 23:32:08 +0200

    Rework Credits overlay.

--- source/overlays/creditsOverlay.cpp
+++ source/overlays/creditsOverlay.cpp
@@ -26,7 +26,7 @@
 
 #include "overlay.hpp"
 
-static void Draw(void) {
+static void Draw(int page) {
 	Gui::clearTextBufs();
 	C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 	C2D_TargetClear(Top, C2D_Color32(0, 0, 0, 0));
@@ -34,29 +34,50 @@ static void Draw(void) {
 
 	UI::DrawBase(true, true);
 	UI::DrawSprite(sprites_bottom_bar_idx, 0, 209);
+	UI::DrawSprite(sprites_stackz_idx, 130, 74);
 	Gui::DrawStringCentered(0, -2, 0.9f, C2D_Color32(255, 255, 255, 255), "Credits", 395, 0, fnt);
-
-	Gui::DrawStringCentered(0, 28, 0.8f, C2D_Color32(0, 0, 0, 255), "SuperSaiyajinStackZ", 395, 0, fnt);
-	Gui::DrawStringCentered(0, 48, 0.7f, C2D_Color32(0, 0, 0, 255), "- For creating this app.", 395, 0, fnt);
-	Gui::DrawStringCentered(0, 78, 0.8f, C2D_Color32(0, 0, 0, 255), "All LeafEdit-Core contributors", 395, 0, fnt);
-	Gui::DrawStringCentered(0, 98, 0.7f, C2D_Color32(0, 0, 0, 255), "- For helping on LeafEdit-Core.", 395, 0, fnt);
-	Gui::DrawStringCentered(0, 128, 0.8f, C2D_Color32(0, 0, 0, 255), "devkitPro", 395, 0, fnt);
-	Gui::DrawStringCentered(0, 148, 0.7f, C2D_Color32(0, 0, 0, 255), "- For Libctru, Citro2D & Citro3D.", 395, 0, fnt);
-	Gui::DrawStringCentered(0, 178, 0.8f, C2D_Color32(0, 0, 0, 255), "Universal-Team", 395, 0, fnt);
-	Gui::DrawStringCentered(0, 198, 0.7f, C2D_Color32(0, 0, 0, 255), "- For LeafEdit's Pattern core and Universal-Core.", 395, 0, fnt);
-	Gui::DrawString(395-Gui::GetStringWidth(0.8, V_STRING, fnt), 219, 0.8, C2D_Color32(255, 255, 255, 255), V_STRING, 400, 0, fnt);
+	Gui::DrawStringCentered(0, 30, 0.9f, C2D_Color32(0, 0, 0, 255), "Main Developer: SuperSaiyajinStackZ", 390, 0, fnt);
+	Gui::DrawString(395 - Gui::GetStringWidth(0.8, V_STRING, fnt), 219, 0.8, C2D_Color32(255, 255, 255, 255), V_STRING, 400, 0, fnt);
 
 	UI::DrawBase(false, true);
+	UI::DrawSprite(sprites_top_bar_idx, 0, 0);
+	Gui::DrawStringCentered(0, -2, 0.9f, C2D_Color32(255, 255, 255, 255), "Page: " + std::to_string(page + 1) + " | 2", 395, 0, fnt);
+
+	if (page == 0) {
+		Gui::DrawStringCentered(0, 28, 0.8f, C2D_Color32(0, 0, 0, 255), "All LeafEdit-Core contributors", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 48, 0.7f, C2D_Color32(0, 0, 0, 255), "- For helping on LeafEdit-Core.", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 78, 0.8f, C2D_Color32(0, 0, 0, 255), "devkitPro", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 98, 0.7f, C2D_Color32(0, 0, 0, 255), "- For Libctru, Citro2D & Citro3D.", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 128, 0.8f, C2D_Color32(0, 0, 0, 255), "Slattz", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 148, 0.7f, C2D_Color32(0, 0, 0, 255), "- For the game icons and region flags from NLTK.", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 178, 0.8f, C2D_Color32(0, 0, 0, 255), "SuperSaiyajinStackZ", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 198, 0.7f, C2D_Color32(0, 0, 0, 255), "- For creating this app.", 395, 0, fnt);
+	} else {
+		Gui::DrawStringCentered(0, 28, 0.8f, C2D_Color32(0, 0, 0, 255), "TotallyNotGuy", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 48, 0.7f, C2D_Color32(0, 0, 0, 255), "- For LeafEdit's awesome Graphics.", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 78, 0.8f, C2D_Color32(0, 0, 0, 255), "Universal-Team", 395, 0, fnt);
+		Gui::DrawStringCentered(0, 98, 0.7f, C2D_Color32(0, 0, 0, 255), "- For LeafEdit's Pattern core and Universal-Core.", 395, 0, fnt);
+	}
+	
 	C3D_FrameEnd(0);
 }
 
 void Overlays::CreditsOverlay() {
 	bool doOut = false;
+	int page = 0;
 
 	while(!doOut) {
-		Draw();
+		Draw(page);
 		hidScanInput();
 
-		if (hidKeysDown()) doOut = true;
+		if (hidKeysDown() & KEY_B) doOut = true;
+
+		if (hidKeysDown() & KEY_R || hidKeysDown() & KEY_RIGHT) {
+			page = 1;
+		}
+
+		if (hidKeysDown() & KEY_L || hidKeysDown() & KEY_LEFT) {
+			page = 0;
+		}
 	}
 }
